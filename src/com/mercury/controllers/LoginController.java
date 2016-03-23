0bceacd71b8513d6035d07ee55b9bc73c5fb8902
wbfeng1987 <wbfeng1987@gmail.com>
2016-03-23 01:10:03 -0400

    login initial test
    Spring security works. Trying to write a login page, bootstrap installed

--- src/com/mercury/controllers/LoginController.java
+++ src/com/mercury/controllers/LoginController.java
@@ -1,8 +1,15 @@
 package com.mercury.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
+	//for login
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
 }
