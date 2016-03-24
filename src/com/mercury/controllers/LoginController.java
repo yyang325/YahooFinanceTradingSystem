586e796dc3a0397fae732852a9d806e648cdf48e
unknown <>
2016-03-24 14:32:07 -0400

    before doing login and safety, CSV util has been added and tested

--- src/com/mercury/controllers/LoginController.java
+++ src/com/mercury/controllers/LoginController.java
@@ -1,10 +1,25 @@
 package com.mercury.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.mercury.services.UserService;
 
 @Controller
 @SessionAttributes
 public class LoginController {
+	
+	@Autowired
+	private UserService us;
+	
+	//for login
+	@RequestMapping(value="login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+		return "login";
+	}
 
 }
--- src/com/mercury/util/CsvUtil.java
+++ src/com/mercury/util/CsvUtil.java
@@ -2,6 +2,7 @@ package com.mercury.util;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -11,12 +12,15 @@ import javax.servlet.ServletContext;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.omg.CORBA.portable.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+//import com.mercury.beans.Transaction;
+import com.mercury.beans.UserStockTransaction;
 //import com.mercury.beans.Transaction;
 import com.mercury.daos.StockDao;
 import com.mercury.daos.UserDao;
@@ -37,6 +41,8 @@ public class CsvUtil {
 	@Autowired
 	ServletContext servletContext;
 	
+	String filePath;
+	
 	/**
 	 * test readCSV() function
 	 * @return
@@ -55,16 +61,11 @@ public class CsvUtil {
 			File f = new File(filePath);
 			
 			System.out.println(f.exists());
-//			
-//			if(f != null) System.out.println("read OK");
-//			else System.out.println("read wrong");    
-//			
+
 			FileReader fr = new FileReader(f);
 			CSVParser parser = new CSVParser(fr, CSVFormat.DEFAULT);
 		   
-		    //CSVParser parser = new CSVParser(null, null);
 		    List<CSVRecord> l = parser.getRecords();
-		    //List<CSVRecord> l = parser.getRecords();
 			for (CSVRecord r:l){
 				sb.append(r.get(0));
 			}
@@ -82,34 +83,77 @@ public class CsvUtil {
 	 * @return --  a list of pending transactions
 	 */
 	//@Transactional
-//	public List<Transaction> parseCSV(){
-//		List<Transaction> list = new ArrayList<Transaction>();
-//		try{
-//		    
-//			String filePath = servletContext.getRealPath("WEB-INF/csv/pending.csv");
-//			
-//			File f = new File(filePath);
-//
-//			FileReader fr = new FileReader(f);
-//			CSVParser parser = new CSVParser(fr, CSVFormat.DEFAULT);
-//		    
-//		    
-//			List<CSVRecord> l = parser.getRecords();
-//			for (CSVRecord r:l){
-//				Transaction ts = new Transaction();
-////				ts.setUser(ud.findByUid(Integer.parseInt(r.get(0))));
-////				ts.setStock(sd.findBySid(Integer.parseInt(r.get(1))));
-////				ts.setAmount(Integer.parseInt(r.get(2)));
-////				ts.setPrice(new BigDecimal(r.get(3)));
-////				ts.setTs(Timestamp.valueOf(r.get(4)));
-////				list.add(ts);
-//			}
-//			fr.close();
-//			parser.close();
-//			
-//		}catch (Exception e){
-//			e.printStackTrace();
-//		}
-//		return list;
-//	}
+	public List<UserStockTransaction> parseCSV(){
+		List<UserStockTransaction> list = new ArrayList<UserStockTransaction>();
+		try{
+		    
+			filePath = servletContext.getRealPath("WEB-INF/csv/pending.csv");
+			
+			File f = new File(filePath);
+
+			FileReader fr = new FileReader(f);
+			CSVParser parser = new CSVParser(fr, CSVFormat.DEFAULT);
+		    
+		    
+			List<CSVRecord> l = parser.getRecords();
+			for (CSVRecord r:l){
+				UserStockTransaction ts = new UserStockTransaction();
+//				ts.setUser(ud.findByUid(Integer.parseInt(r.get(0))));
+//				ts.setStock(sd.findBySid(Integer.parseInt(r.get(1))));
+//				ts.setAmount(Integer.parseInt(r.get(2)));
+//				ts.setPrice(new BigDecimal(r.get(3)));
+//				ts.setTs(Timestamp.valueOf(r.get(4)));
+//				list.add(ts);
+			}
+			fr.close();
+			parser.close();
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	/**
+	 * this method is used to append pending transactions to the csv file
+	 * @param trans
+	 */
+	public void appendCSV(UserStockTransaction trans){
+		try{
+			
+			filePath = servletContext.getRealPath("WEB-INF/csv/pending.csv");
+			File f = new File(filePath);
+			
+			FileWriter fw = new FileWriter(f, true);			
+			CSVPrinter cp = new CSVPrinter(fw, CSVFormat.DEFAULT);
+			cp.printRecord((Object[]) trans.toString().split(","));
+			fw.flush();
+			fw.close();
+			cp.close();			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * this method is used to rewrite the csv file for all the pending transactions
+	 * @param trans
+	 */
+	public void rewriteCSV(UserStockTransaction trans){
+		try{
+			
+			filePath = servletContext.getRealPath("WEB-INF/csv/pending.csv");
+			File f = new File(filePath);
+			
+			FileWriter fw = new FileWriter(f);			
+			CSVPrinter cp = new CSVPrinter(fw, CSVFormat.DEFAULT);
+			cp.printRecord((Object[]) trans.toString().split(","));
+			fw.flush();
+			fw.close();
+			cp.close();			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
 }
